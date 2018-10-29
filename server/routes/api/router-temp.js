5bee7131347defa587062ccc7073b960a1932f15
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2018-10-28 21:34:07 -0500

    trying to get temperature. having issues with the api incorrectly reporting connectivity of devices

--- server/routes/api/router-temp.js
+++ server/routes/api/router-temp.js
@@ -1,9 +1,21 @@
 const router = require('express').Router()
 const { getAllDevices, getConnectedDevices } = require('../../modules/particle/particleDevices')
+const particle = require('../../modules/particle/particle')
+
+const token = process.env.PARTICLE_TOKEN
+console.log(token)
 
 router.get('/',(req,res) => {
-  const devices = getConnectedDevices()
-  .then(devices => res.send(devices))
+  getConnectedDevices()
+  .then(devices => {
+    const varCalls = devices.map(device => {
+      console.log(device)
+      return particle.getVariable({deviceId: device.id, name: 'tempC', auth: token}).catch(err => console.log(err))
+    })
+    Promise.all(varCalls)
+    .then(responses => res.send(responses))
+    .catch(err => res.send(err.error))
+  })
 })
 
 module.exports = router
